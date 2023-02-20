3564fd2efe02db0c7a55cd7d39822f4ca8aa38de
Herbi Tarik <herbitarik2509@gmail.com>
2023-02-20 20:33:24 +0100

    added safeAreaView and splashScreen

---
+++ clone whatsApp/clone-whatsApp/App.js
@@ -0,0 +1,30 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+import { SafeAreaProvider, SafeAreaView } from 'react-native-safe-area-context';
+
+
+export default function App() {
+
+
+
+
+  return (
+    <SafeAreaProvider style={styles.container}>
+    <SafeAreaView>
+    <StatusBar style="auto" />
+    
+    
+
+    </SafeAreaView>
+    </SafeAreaProvider>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    justifyContent: 'center',
+    alignItems: 'center'
+  },
+});
