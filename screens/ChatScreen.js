bc0131b4548f2264915357add0080de1e75da63e
Herbi Tarik <herbitarik2509@gmail.com>
2023-02-22 10:58:22 +0100

    Showing a send button as the user types

--- screens/ChatScreen.js
+++ screens/ChatScreen.js
@@ -3,8 +3,13 @@ import backgroundImage from "../assets/images/droplet.jpeg";
 import { SafeAreaView } from 'react-native-safe-area-context';
 import { Feather } from '@expo/vector-icons';
 import colors from '../constants/colors';
+import { useState } from 'react';
 
 const ChatScreen = ({navigation}) => {
+
+    const  [messageText, setMessageText] = useState("");
+
+    console.log(messageText);
   return (
    <SafeAreaView edges={['right','left','buttom']} style={styles.container}>
     <ImageBackground source={backgroundImage} style={styles.backgroundImage}>
@@ -18,14 +23,22 @@ const ChatScreen = ({navigation}) => {
             <Feather name="plus" size={24} color={colors.blue} />
         </TouchableOpacity>
 
-        <TextInput style={styles.textbox} />
+        <TextInput style={styles.textbox} value={messageText} onChangeText={(e) => setMessageText(e)} />
 
+        {messageText === "" &&
         <TouchableOpacity 
             style={styles.mediaButton}
             onPress={() => console.log("pressed!")}>
             <Feather name="camera" size={24} color={colors.blue} />
         </TouchableOpacity>
-        
+        }
+        {messageText !== "" &&
+        <TouchableOpacity 
+            style={styles.mediaButton}
+            onPress={() => console.log("pressed!")}>
+            <Feather name="send" size={24} color={colors.blue} />
+        </TouchableOpacity>
+        } 
     </View>
    </SafeAreaView>
   )
